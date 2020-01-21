3a3e00a3a3177b9e03c34fc8eceeacd715613ba4
Alessandro_Mondelli <alexs98@hotmail.it>
2020-01-21 16:34:30 +0100

    Completato stile header

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Google FAQ</title>
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="public/app.css">
+    </head>
+
+    <body>
+        <header>
+            <div id="google-head">
+                <div id="up-logo">
+                    <a href="#"><img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="logo"></a>
+                    <a href="#">Privacy e termini</a>
+                </div>
+                <div id="nav">
+                    <ul>
+                        <li><a href="#">Introduzione</a></li>
+                        <li><a href="#">Norme sulla privacy</a></li>
+                        <li><a href="#">Termini di servizio</a></li>
+                        <li><a href="#">Tecnologie</a></li>
+                        <li><a href="#">Domande frequenti</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="faq-sez container">
+                <div class="faq">
+                    <h2>Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?</h2>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
